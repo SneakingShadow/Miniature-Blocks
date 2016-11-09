1ed47f09c0b6caca1dcd7d78d4dc6b6ea2571f2b
SneakingShadow <thesneakingshadow@gmail.com>
2016-11-09 20:51:44 +0100

    Starting up

---
+++ src/main/java/com/sneakingshadow/miniatureblocks/Reference/Ref.java
@@ -0,0 +1,15 @@
+package com.sneakingshadow.miniatureblocks.Reference;
+
+/**
+ * Created by SneakingShadow on 09.11.2016.
+ */
+public class Ref {
+
+    public static final String MOD_ID = "shadow_miniature_blocks";
+    public static final String MOD_NAME = "Shadow's Miniature Blocks";
+    public static final String VERSION = "1.7.10-0.0.1";
+    private static final String ROOT_FOLDER = "com.sneakingshadow.miniatureblocks";
+    public static final String SERVER_PROXY_CLASS = ROOT_FOLDER+".proxy.ServerProxy";
+    public static final String CLIENT_PROXY_CLASS = ROOT_FOLDER+".proxy.ClientProxy";
+
+}
